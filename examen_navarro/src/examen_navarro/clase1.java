5a4fd5e50893ae039fe64ba855be6118ae73a62f
naavarro7 <mcsnt884@gmail.com>
2018-02-27 09:53:39 +0100

    Modificacio

--- examen_navarro/src/examen_navarro/clase1.java
+++ examen_navarro/src/examen_navarro/clase1.java
@@ -4,15 +4,12 @@ public class clase1 {
 public static void main(String[]args){
 	printOne();
 	printOne();
-	printTwo();
+	
 	
 	
 }
 public static void printOne() {
 	System.out.println("hello world");
 }
-public static void printTwo() {
-	printOne();
-	printOne();
-}
+
 }
