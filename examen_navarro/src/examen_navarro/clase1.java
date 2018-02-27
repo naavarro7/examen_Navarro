8b6513de8b317f460c4d75fcd2c093b22a10d13e
naavarro7 <mcsnt884@gmail.com>
2018-02-27 09:50:31 +0100

    pujada 1

---
+++ examen_navarro/src/examen_navarro/clase1.java
@@ -0,0 +1,18 @@
+package examen_navarro;
+
+public class clase1 {
+public static void main(String[]args){
+	printOne();
+	printOne();
+	printTwo();
+	
+	
+}
+public static void printOne() {
+	System.out.println("hello world");
+}
+public static void printTwo() {
+	printOne();
+	printOne();
+}
+}
